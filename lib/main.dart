99b52aa7e4d6882e30c303de153d4d7b0d7a8ae0
Jorge Hernandez <jhackhdez@gmail.com>
2023-07-05 15:27:25 -0500

    create home and details screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_films/screens/screens.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Films App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen()
+      },
     );
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_films/screens/details_screen.dart';
+export 'package:flutter_films/screens/home_screen.dart';
